9a7ccaa5cfd6bf852549ff70159791d2d5f04cee
王刚 <wanggang@zxisl.com>
2015-09-02 12:32:56 +0800

    初始化项目

---
+++ bin/package.bat
@@ -0,0 +1,19 @@
+@echo off
+rem /**
+rem  * Copyright &copy; 2012-2014 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+rem  *
+rem  * Author: ThinkGem@163.com
+rem  */
+echo.
+echo [信息] 打包工程，生成war包文件。
+echo.
+pause
+echo.
+
+cd %~dp0
+cd..
+
+call mvn clean package -Dmaven.test.skip=true
+
+cd bin
+pause
